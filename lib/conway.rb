13958d61cb1d931520ddcabed5f4a23ebf610d8c
Anthony Broccoli <acbrocco@syr.edu>
2015-01-20 15:35:36 -0500

    code for pingpongness

---
+++ lib/conway.rb
@@ -0,0 +1,15 @@
+class Cell
+  def initialize
+    @dead_alive? = Random.rand(0..1)
+  end
+end
+
+
+class GameOfLife
+
+  def initialize(newgame)
+    @newgame = newgame
+  end
+
+
+end
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,1 +1,24 @@
 require_relative '../lib/conway'
+
+
+describe Cell do
+  it 'create a new cell randomly alive or dead' do
+    expect(Cell.new).to eq 0 || 1
+  end
+end
+
+
+describe GameOfLife do
+  subject(:newgame) {GameOfLife.new([])}
+  describe '#initialize' do
+    it 'initialize new intance of game of life' do
+      expect(newgame).to be_a GameOfLife
+    end
+  end
+
+  # describe '#grid' do
+  #   it 'creates a grid with an array of arrays' do
+  #     expect(grid.newgame).to eq [[]]
+
+
+end
